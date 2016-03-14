ef34e5ae6ec23d9e085feffc3452a37975d3ea22
Thomas Zurch <zurch@appstoremedia.com>
2016-03-14 23:04:35 +0000

    corrected mispelled password in invironment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :port => '587',
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASWORD'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :enable_startstls_auto => true
 }
