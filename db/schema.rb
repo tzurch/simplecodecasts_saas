a26e5ad31ce2da18565bc8a11e6a653489358661
Thomas Zurch <zurch@appstoremedia.com>
2016-03-23 20:57:55 +0000

    Added assoc for plan and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323201202) do
+ActiveRecord::Schema.define(version: 20160323204943) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160323201202) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
