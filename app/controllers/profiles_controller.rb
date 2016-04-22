574aaebb68490fbaeb433cc656e771b17c592b61
Thomas Zurch <zurch@appstoremedia.com>
2016-04-22 19:48:11 +0000

    Added profile forms and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,23 @@
 class ProfilesController < ApplicationController
-    
+  def new
+    #form for user to fill out their own profile.
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile
+  end
+  
+  def create
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if profile.save
+      flash[:success] = "Yea! Profile Updated"
+      redirect_to user_path( params[:user_id] ) 
+    else
+      render action: :new
+    end  
+  end
+  
+  private
+    def profile_params
+      params require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end
